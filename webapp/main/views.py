f7e50409e235d0b1dd0432f295bf83441831a995
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-03-01 04:01:46 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@20 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_page = Blueprint('main_page', __name__)
 
 @main_page.route('/')
 def index():
-    return "welcome index"
+    return render_template('/common/head.html')
