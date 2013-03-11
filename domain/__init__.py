8b6abc257090bbbaf385bb61e7ebdc0053beccee
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-03-11 02:15:49 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@55 288da536-3c93-4ab2-bc2e-17a84f795b60

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,4 +1,5 @@
-import sae.const 
+import sae.const
+import sqlalchemy
 from sqlalchemy import create_engine
 
 engine = create_engine('mysql+mysqldb://' + sae.const.MYSQL_USER + ':' + sae.const.MYSQL_PASS + '@' + sae.const.MYSQL_HOST + '/' + sae.const.MYSQL_DB)
