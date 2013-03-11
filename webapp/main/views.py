deeb318e641ca772030fea10a578f8facd277974
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-03-11 01:44:26 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@49 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from domain import get_version
 
 main_page = Blueprint('main_page', __name__)
 
 @main_page.route('/')
 def index():
-    return render_template('/layout/main.html')
+    version = get_version()
+    return render_template('/layout/main.html',
+        version=version,
+    )
