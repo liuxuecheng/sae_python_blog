52667e6ed92823e9288557240468336e21b85785
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-03-12 01:44:41 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@61 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/mydb/db.py
+++ webapp/mydb/db.py
@@ -1,7 +1,16 @@
 from flask import Blueprint
+from domain import engine
+from domain.moder import Base
 
 db_page = Blueprint('db_page', __name__)
 
 @db_page.route('/mydb/createdb')
 def create_db():
+	"""
+	create all db
+	"""
+
+	from domain.model.user import User
+	Base.metadata.create_all(engine) 
+	
 	return 'create db'
