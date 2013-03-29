653b554c8306936dabda46bb5809302f7a4864f9
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-03-29 02:24:47 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@164 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-user_page = Buleprint("user_page", __name__)
+user_page = Blueprint("user_page", __name__)
 
 @user_page.route("/user/login", methods=("POST","GET"))
 def login():
