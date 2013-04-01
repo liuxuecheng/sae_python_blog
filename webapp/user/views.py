38eb55599367fa2a9b451365bb28eff849e8dc5e
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-01 01:51:22 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@180 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -6,3 +6,9 @@ user_page = Blueprint("user_page", __name__)
 def login():
 
 	return render_template("/user/login.html")
+
+
+@user_page.route("/user/register", methods=('POST','GET'))
+def register():
+	
+	return render_template('/user/register.html')	
