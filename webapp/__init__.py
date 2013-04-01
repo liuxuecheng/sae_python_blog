6454cd3a402241d41080aebc694d6ea034a54224
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-01 10:17:10 +0000

    add the setting.ini file
    git-svn-id: https://svn.sinaapp.com/dilan/2@188 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,7 @@ from webapp.user.views import user_page
 
 #app config
 app = Flask(__name__)
-app.config.from_pyfile('setting.py')
+app.config.from_pyfile('setting.ini')
 
 #register app
 app.register_blueprint(main_page)
