41119a4648dfee17bbac53d479acc04a0b264d1f
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 10:32:55 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@232 288da536-3c93-4ab2-bc2e-17a84f795b60

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,6 +1,7 @@
 import sae.const
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from domain.model import Base
 
 db_link = 'mysql+mysqldb://' + sae.const.MYSQL_USER + ':' + sae.const.MYSQL_PASS + '@' + sae.const.MYSQL_HOST + ':' + sae.const.MYSQL_PORT + '/' + sae.const.MYSQL_DB
 
