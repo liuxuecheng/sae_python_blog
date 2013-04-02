b3331124c9e705503c77e79a66d0399af7bc31fd
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 03:06:31 +0000

    add the wtforms package
    git-svn-id: https://svn.sinaapp.com/dilan/2@199 288da536-3c93-4ab2-bc2e-17a84f795b60

--- package/wtforms/__init__.py
+++ package/wtforms/__init__.py
@@ -8,9 +8,9 @@ development.
 :copyright: Copyright (c) 2010 by Thomas Johansson, James Crasta and others.
 :license: BSD, see LICENSE.txt for details.
 """
-from wtforms import validators, widgets
-from wtforms.fields import *
-from wtforms.form import Form
-from wtforms.validators import ValidationError
+from package.wtforms import validators, widgets
+from package.wtforms.fields import *
+from package.wtforms.form import Form
+from package.wtforms.validators import ValidationError
 
 __version__ = '1.0.3'
