0c8611377ed920ee6e24f42d586a438295c4e576
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 03:12:37 +0000

    add the wtforms package
    git-svn-id: https://svn.sinaapp.com/dilan/2@203 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/form.py
+++ webapp/user/form.py
@@ -1,4 +1,5 @@
-from package.wtforms import Form, TextField, validators, Required
+from package.wtforms import Form, TextField, validators
+from package.wtforms.validators import Required
 
 class LoginForm(Form):
 	email = TextField('email', validators=[
