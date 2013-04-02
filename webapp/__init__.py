113aa8b33935946f5418e3a1c9e548fe3648761d
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 02:25:09 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@193 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,15 +4,18 @@ from webapp.main.views import main_page
 from webapp.mydb.db import db_page
 from webapp.user.views import user_page
 
+
 #app config
 app = Flask(__name__)
 app.config.from_pyfile('setting.ini')
 app.config.from_envvar('SETTING', silent=True)
 
-#register app
+
+#register blueprint
 app.register_blueprint(main_page)
 app.register_blueprint(db_page)
 app.register_blueprint(user_page)
+
 
 #app before request
 @app.before_request
