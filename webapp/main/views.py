d89d92ad6850a57c06edee539a4bfd98579e0c45
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 10:15:19 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@227 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -7,7 +7,7 @@ main_page = Blueprint('main_page', __name__)
 @main_page.route('/')
 def index():
 	user_info = User.query.all()
-    return render_template('/sites/index.html')
+	return render_template('/sites/index.html')
 
 
 @main_page.route('/about')
