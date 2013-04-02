0b8afde0c6ff11053638d3570aabab008c18c9bd
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 10:17:37 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@228 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, render_template
 from domain.model.user import User
+from domain.model import db_session
 
 main_page = Blueprint('main_page', __name__)
 
 
 @main_page.route('/')
 def index():
-	user_info = User.query.all()
+	user_info = db_session.query(User).all()
 	return render_template('/sites/index.html')
 
 
