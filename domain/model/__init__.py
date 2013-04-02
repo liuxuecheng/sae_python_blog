c4cc709374a2b20109163b7e16e7f91f8e2901c4
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 10:02:11 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@225 288da536-3c93-4ab2-bc2e-17a84f795b60

--- domain/model/__init__.py
+++ domain/model/__init__.py
@@ -1,3 +1,41 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
 
-Base = declarative_base()
+class _Base(object):
+    """
+    Custom base model.
+    """
+
+    @classmethod
+    def get(cls, id):
+        """
+        Get object by it's id.
+        """
+
+        return cls.query.get(id)
+
+    @classmethod
+    def gets(cls, ids):
+        """
+        Get objects by id list.
+        """
+
+        ids = {}.fromkeys(ids).keys()
+        if len(ids) > 0:
+            return cls.query.filter(cls.id.in_(ids))
+        else:
+            return []
+
+    @declared_attr
+    def __tablename__(cls):
+        """
+        Automatic cover model map tablename to lower
+        """
+
+        return cls.__name__.lower()
+
+    # Default table config.
+    __table_args__ = {'mysql_charset': 'utf8',
+                      'mysql_engine': ''}
+
+
+Base = declarative_base(cls=_Base)
