aaabec1b32df640680cf0d54bf895bebd96bd308
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-02 02:32:54 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@195 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, render_template
+from webapp.user.form import LoginForm
+
 
 user_page = Blueprint("user_page", __name__)
-
-@user_page.route("/user/login", methods=("POST","GET"))
-def login():
-
-	return render_template("/user/login.html")
 
 
 @user_page.route("/user/register", methods=('POST','GET'))
 def register():
-	
 	return render_template('/user/register.html')	
+
+
+@user_page.route("/user/login", methods=("POST","GET"))
+def login():
+	login_form = LoginForm()
+	return render_template("/user/login.html")
+
+
+@user_page.route("/user/logout", methods=('GET'))
+def logout():
+	pass
