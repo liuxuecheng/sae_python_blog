a75462a72cf6fcc283dee8cae8766967056a87fb
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-03 03:11:21 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@242 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/form.py
+++ webapp/user/form.py
@@ -1,30 +1,47 @@
 from package.wtforms import Form, TextField, validators
 from package.wtforms.validators import Required, Length, ValidationError, Email
 from domain.model.user import User
+from flask import flash
 
 
 class LoginForm(Form):
-
-
-	def check_email(form, field):
-		user_info = User.query.filter(User.email == field.data).first()
-		if  user_info is None:
-			raise ValidationError('The user does not exist !')
-
-
-	def check_password(form, field):
-		user_info = User.query.filter(User.email == form.email.data).first()
-		if  user_info.password != field.data: 
-			raise ValidationError('Password is wrong !')
-
-
 	email = TextField('email', validators=[
 			Required('Email not empty!'),
 			Email('Email format is wrong !'),
-			check_email
 		])
 	password = TextField('password', validators=[
 			Required('password not empty !'),
 			Length(min=5, max=18),
-			check_password
 		])
+
+
+	def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.user = None
+
+
+    def validate(self):
+        """
+        Validate user email and password is correct.
+        """
+
+        if not Form.validate(self):
+            return False
+
+        user = User.query.filter(User.email == self.email.data).first()
+
+        if user is not None:
+            if user.password == self.password.data:
+                self.user = user
+                is_user_valid = True
+            else:
+                self.user = None
+                is_user_valid = False
+                flash(u'邮箱或密码错误', 'error')
+
+        else:
+            self.user = None
+            is_user_valid = False
+            flash(u'邮箱或密码错误', 'error')
+
+        return is_user_valid
