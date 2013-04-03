d3a6f316e77b6a5d257b846983becc8a44c4b34d
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-03 02:57:01 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@239 288da536-3c93-4ab2-bc2e-17a84f795b60

--- domain/__init__.py
+++ domain/__init__.py
@@ -5,6 +5,6 @@ from domain.model import Base
 
 db_link = 'mysql+mysqldb://' + sae.const.MYSQL_USER + ':' + sae.const.MYSQL_PASS + '@' + sae.const.MYSQL_HOST + ':' + sae.const.MYSQL_PORT + '/' + sae.const.MYSQL_DB
 
-engine = create_engine(db_link, pool_recycle=3600)
+engine = create_engine(db_link, pool_recycle=30)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base.query = db_session.query_property()
