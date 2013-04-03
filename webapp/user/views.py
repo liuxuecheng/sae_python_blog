6e725920690c7ad7e62486e004e1605ef5bb2f83
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-03 09:28:52 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@258 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, g
+from flask import Blueprint, render_template, request, g, session, redirect
 from webapp.user.form import LoginForm
+from domain.model.user import User
 
 
 user_page = Blueprint("user_page", __name__)
@@ -12,19 +13,29 @@ def register():
 
 @user_page.route("/user/login", methods=("POST","GET"))
 def login():
-	user = []
 
 	login_form = LoginForm(request.form)
 	if request.method == 'POST':
 		if login_form.validate():
-			user = login_form.user
-	
+			session['user_id'] = login_form.user.user_id
+			return redirect('/user')
+
 	return render_template("/user/login.html",
-			loginform=login_form,
-			user=user
+			loginform=login_form
 		)
 
 
 @user_page.route("/user/logout", methods=('GET'))
 def logout():
 	pass
+
+
+@user_page.route("/user")
+def user():
+	if 'user_id' in session:
+		user = User.query.filter(User.id == session['user_id']).first()
+	else:
+		user = []	
+	return render_template('/user/index.html',
+			user = uesr
+		)	
