250fcb98f27962748e25be73606f8b0c7aba6b72
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-07 08:35:01 +0000

    add the admin index.html file
    git-svn-id: https://svn.sinaapp.com/dilan/2@310 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/__init__.py
+++ webapp/user/__init__.py
@@ -5,8 +5,8 @@ def login_required(func):
 	@wraps(func)
 	def _(*args, **kwargs):
 		if g.user is None:
-			return abort(404)
-			#return redirect('/user/login')
+			#return abort(404)
+			return redirect('/user/login')
 		return func(*args, **kwargs)
 
 	return _	
