d5e790c73ef66ebf8b458bb30e52b03dfa40c5a3
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-07 08:33:48 +0000

    add the admin index.html file
    git-svn-id: https://svn.sinaapp.com/dilan/2@308 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/__init__.py
+++ webapp/user/__init__.py
@@ -4,7 +4,7 @@ from flask import g, request, redirect, abort
 def login_required(func):
 	@wraps(func)
 	def _(*args, **kwargs):
-		if g.user.id == '':
+		if g.user.get(id) is None:
 			return abort(404)
 			#return redirect('/user/login')
 		return func(*args, **kwargs)
