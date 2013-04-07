e08872b94ab511f2c9a774102114c779cd8c3959
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-07 10:05:49 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@329 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/admin/form.py
+++ webapp/admin/form.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from package.wtforms import Form, TextField, validators
+from package.wtforms import Form, TextField, validators, HiddenField, IntegerField
 from package.wtforms.validators import Required, Length, ValidationError, Email
 
 
 class CategoryForm(Form):
+	id = HiddenField('id', default=0)
+	parent_id  = IntegerField('parent_id', default=0)
+	name = TextField('name', default='')
+	priority = IntegerField('priority', default=0)
--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template, jsonify
 from domain.model.user import User
 from domain.model.topic import Category, Topic, TopicTag
 from domain import db_session
-from webapp.user import login_required, is_admin 
+from webapp.user import login_required, is_admin
+from webapp.admin.form import CategoryForm 
 
 
 admin_page = Blueprint('admin_page', __name__)
@@ -20,7 +21,10 @@ def index():
 @login_required
 @is_admin
 def category():
-	return render_template('/admin/category.html')
+	category_form = CategoryForm()
+	return render_template('/admin/category.html',
+		category_form = category_form	
+		)
 
 
 @admin_page.route('/admin/topic/addcategory')
