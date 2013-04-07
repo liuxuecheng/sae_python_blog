a5ca30db3575da65074a752a76d0f6a8e506c8ce
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-07 02:43:06 +0000

    up
    git-svn-id: https://svn.sinaapp.com/dilan/2@269 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, g
+from flask import Flask, g, session
 from webapp.main.views import main_page
 from webapp.mydb.db import db_page
 from webapp.user.views import user_page
