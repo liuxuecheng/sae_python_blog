7a5974fb4c608f5184c36eeb10d749aa165804ce
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-08 01:06:42 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@343 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/__init__.py
+++ webapp/user/__init__.py
@@ -6,7 +6,7 @@ def login_required(func):
 	@wraps(func)
 	def _(*args, **kwargs):
 		if g.user is None:
-			return redirect(url_for('user.login'))
+			return redirect(url_for('user_page.login'))
 		return func(*args, **kwargs)
 
 	return _
