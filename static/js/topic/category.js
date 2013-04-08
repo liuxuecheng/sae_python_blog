49f94395027fcad9e3df482a8ef62603925775a4
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-08 07:25:41 +0000

    add the category.js
    git-svn-id: https://svn.sinaapp.com/dilan/2@375 288da536-3c93-4ab2-bc2e-17a84f795b60

--- static/js/topic/category.js
+++ static/js/topic/category.js
@@ -1,6 +1,17 @@
 var doSave = $("#doSave");
 
+function addcategory(){
+	$.ajax({
+		type:'POST',
+		url:'/admin/addcategory',
+		dataType:'json',
+		data:$("categoryForm").serialize(),
+		success:function(data){
+			alert(data.code);
+		}
+	})
+}
 
 doSave.click(function(){
-	alert(1);
+	addcategory()
 })
