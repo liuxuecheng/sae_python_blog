111ebf02edb9f66b027fba23146af5e39c70cf74
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-08 00:43:41 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@335 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/user/__init__.py
+++ webapp/user/__init__.py
@@ -1,12 +1,12 @@
 from functools import wraps 
-from flask import g, request, redirect, abort
+from flask import g, request, redirect, abort, url_for
 
 
 def login_required(func):
 	@wraps(func)
 	def _(*args, **kwargs):
 		if g.user is None:
-			return redirect('/user/login')
+			return redirect(request.args.get('next') or url_for('index'))
 		return func(*args, **kwargs)
 
 	return _
