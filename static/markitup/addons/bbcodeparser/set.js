3ffc6028d003e0584222b8c4af98ebd25b808e05
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-15 01:34:29 +0000

    del markitup
    git-svn-id: https://svn.sinaapp.com/dilan/2@437 288da536-3c93-4ab2-bc2e-17a84f795b60

--- static/markitup/addons/bbcodeparser/set.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-	$.markItUp.plugin.add('bbcodeParser', {
-		scripts:['scripts/bbcode.js'],
-		onBeforePreviewRefresh:function(miu) {
-			miu.preview.content( parseBBCode( miu.preview.content() ) );
-		}
-	});
-})(jQuery);
--- static/markitup/addons/textileparser/set.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-	$.markItUp.plugin.add('textileParser', {
-		scripts:['scripts/supertextile.js'],
-		onBeforePreviewRefresh:function(miu) {
-			miu.preview.content( superTextile( miu.preview.content() ) );
-		}
-	});
-})(jQuery);
