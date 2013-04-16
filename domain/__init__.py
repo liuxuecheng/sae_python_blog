a65a69dbf18e56266f374f7fb37fd344430b7906
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-16 01:22:45 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@469 288da536-3c93-4ab2-bc2e-17a84f795b60

--- domain/__init__.py
+++ domain/__init__.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from domain.model import Base
 
-db_link = 'mysql+mysqldb://' + sae.const.MYSQL_USER + ':' + sae.const.MYSQL_PASS + '@' + sae.const.MYSQL_HOST + ':' + sae.const.MYSQL_PORT + '/' + sae.const.MYSQL_DB
+db_link = 'mysql+mysqldb://' + sae.const.MYSQL_USER + ':' \
+		+ sae.const.MYSQL_PASS + '@' + sae.const.MYSQL_HOST \
+		+ ':' + sae.const.MYSQL_PORT + '/' + sae.const.MYSQL_DB + '?charset=utf8'
 
 engine = create_engine(db_link, pool_recycle=10)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
