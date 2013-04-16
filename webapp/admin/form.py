3a4a5cd8845c75000e58a31ab2f00f4403c58c3a
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-16 08:40:28 +0000

    update
    git-svn-id: https://svn.sinaapp.com/dilan/2@502 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/admin/form.py
+++ webapp/admin/form.py
@@ -6,9 +6,10 @@ from package.wtforms.validators import Required, Length, ValidationError, Email
 
 class CategoryForm(Form):
 	id = HiddenField('id', default=0)
-	parent_id  = IntegerField('parent_id', default=0)
-	name = TextField('name', default='')
-	priority = IntegerField('priority', default=0)
+	name = TextField('name', validators=[
+		Required(u'分类名称不能为空'),
+		Length(min=2, max=20, message=u'分类名称不能太长')
+		])
 
 
 class TopicForm(Form):
