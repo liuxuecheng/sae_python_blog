68073f0ee8974487cd5bd91aebc1a4e283edd5ca
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-04-26 03:27:41 +0000

    up
    git-svn-id: https://svn.sinaapp.com/dilan/2@638 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, g, session
+from flask import Flask, g, session, abort
 from webapp.mydb.db import db_page
 from webapp.main.views import main_page
 from webapp.user.views import user_page
@@ -38,3 +38,4 @@ def shutdown_session(exception=None):
 	db_session.rollback()
 	db_session.remove()
 	db_session.close()
+	abort(404)
