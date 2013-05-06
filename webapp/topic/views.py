29e11521c742cd15cc5337b66e1c67e82391f153
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-05-06 13:55:12 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@745 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/topic/views.py
+++ webapp/topic/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, g, abort
+from flask import Blueprint, render_template, g, abort, request
 from domain.model.user import User
 from domain import db_session
 from domain.model.topic import Category, Topic, TopicTag, TopicToTag, TopicReply
