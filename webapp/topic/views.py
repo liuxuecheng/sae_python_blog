568cfdedeb47793a6b6ccffebd66676767beed75
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-05-06 09:29:19 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/2@725 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/topic/views.py
+++ webapp/topic/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, g, abort
 from domain.model.user import User
 from domain import db_session
 from domain.model.topic import Category, Topic, TopicTag, TopicToTag
+from webapp.user import login_required
 
 
 topic_page = Blueprint('topic_page', __name__)
@@ -17,6 +18,7 @@ def _():
 @topic_page.route('/topic/<int:topic_id>')
 def show_topic(topic_id):
 	topic = Topic.query.filter(Topic.id == topic_id).first()
+	reply_form = ReplyForm()
 	if topic:
 		category = Category.query.filter(Category.id == topic.category_id).first()
 		topic.count.views += 1
@@ -57,5 +59,6 @@ def tag(tag_name=''):
 
 
 @topic_page.route('/topic/reply', methods=('post',))
+@login_required
 def add_reply():
 	pass			
