7ea12fdd9c195ef8d31f415c19a245ac54c72321
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2013-05-06 09:31:16 +0000

    add the reply form
    git-svn-id: https://svn.sinaapp.com/dilan/2@727 288da536-3c93-4ab2-bc2e-17a84f795b60

---
+++ webapp/topic/form.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from package.wtforms import Form, TextField, validators, HiddenField
+from package.wtforms.validators import Required, Length, ValidationError, Email, EqualTo
+
+
+class ReplyForm(Form):
+	user_id = HiddenField('user_id', validators=[
+			Required(u'user_id不能为空!'),
+		])
+	topic_id = HiddenField('topic_id', validators=[
+			Required(u'topic_id不能为空!'),
+		])
+	content = TextAreaField('content',validators=[
+			Required(u'留言内容不能为空!'),
+		])
